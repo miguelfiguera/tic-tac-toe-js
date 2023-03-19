f357ccca5765699a90ada946d7279a1c2d86fca6
Miguel Figuera <miguelqui725@gmail.com>
2023-03-19 14:32:51 -0400

    Write createPlayer() and styling for index element
    createPlayer function inside a module and styling for the names of the
    players. This way this is done after the user push the submit button.
    Also added the images for picard, janeway and the borg queen.
    Trying to make everything work inside functions or inside modules.
    Avoiding free code all together.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//variables for testing.
+let currentPlayer = {};
+currentPlayer.marker = "Q";
+
 //Div Objects Board - module
 const battlefield = (function () {
   let gameboard = document.getElementById("gameboard");
@@ -7,14 +11,39 @@ const battlefield = (function () {
     div.id = `square${i}`;
     gameboard.appendChild(div);
   }
+
+  const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const janeway = () => {
+    let one = document.getElementById("playerImg1");
+    let two = document.getElementById("playerImg2");
+    one.src = "janeway.jpg";
+    two.src = "Borg_Queen_2372.jpg";
+  };
+
+  const picard = () => {
+    let one = document.getElementById("playerImg1");
+    let two = document.getElementById("playerImg2");
+    one.src = "250.jpg";
+    two.src = "Borg_Queen_2372.jpg";
+  };
+
+  let x = getRandomIntInclusive(1, 2);
+
+  const images = () => {
+    return x == 1 ? picard() : janeway();
+  };
+
+  return {images}
 })();
 
 // eventlisteners and functions
 
-let currentPlayer = {};
-currentPlayer.marker = "Q";
-
-const eventListener = (() => {
+const eventListenerDivs = (() => {
   let divs = document.getElementsByClassName("gameSquare");
   for (let i = 0; i < divs.length; i++) {
     divs[i].addEventListener("click", function () {
@@ -23,20 +52,36 @@ const eventListener = (() => {
         let p = document.createElement("p");
         p.innerText = currentPlayer.marker;
         p.classList.add("playersChoice");
-        p.id="p"+[i]
+        p.id = "p" + [i];
         divs[i].appendChild(p);
-        //every P should have and ID.
         //I should append the players swap here.
         //add a check for winning here also nums of ps go
         //from 0 to 8
       }
     });
   }
-
-  //here you should add the form part
-
-  return { divs };
+  return { divs};
 })();
+
+const createPlayer=(()=>{ 
+  let form=document.getElementById("form")
+  form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    let name1=document.getElementById("name1").value
+    let name2=document.getElementById("name2").value
+    player1=Player(name1,"Q");
+    player2=Player(name2,"X");
+    let nameboard1=document.getElementById("players1name");
+    nameboard1.innerText=player1.getName()
+    let nameboard2=document.getElementById("players2name");
+    nameboard2.innerText=player2.getName()
+    form.classList.add("hidden")
+    battlefield.images()
+    //win count
+  })
+})();
+
+
 
 //Players Object Factory Function
 
@@ -44,7 +89,20 @@ function Player(name, marker) {
   let playerSquares = [];
   let winCount = [];
   let isHuman = true;
-  return { marker, name, winCount, isHuman, playerSquares };
+
+  const getName = () => {
+    return name;
+  };
+  const getWinCount = () => {
+    return winCount;
+  };
+  const getPlayerSquares = () => {
+    return playerSquares;
+  };
+  const getMarker = () => {
+    return marker;
+  };
+  return { getMarker, getName, getWinCount, isHuman, getPlayerSquares };
 }
 
 // handling form
@@ -52,5 +110,8 @@ function Player(name, marker) {
 // game controller (game steps)
 
 const game = (() => {
+
+
+
   return {};
 })();
