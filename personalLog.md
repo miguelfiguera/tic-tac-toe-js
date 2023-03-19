fff0700d2d38892ff762e0924da213d98f74b2e7
Miguel Figuera <miguelqui725@gmail.com>
2023-03-19 15:48:32 -0400

    Modify the player factory function
    This commit adds a little bit of functionality to the object so it gets
    easier to send data to the dom, and to the 'const game'. That way I may
    use the new methods to check for victory and announce the winner, also
    adding points to the score of the game.

--- personalLog.md
+++ personalLog.md
@@ -9,3 +9,19 @@ Objectives:
 4. Style it nicely if possible.
 5. Star Trek themed, because of reasons...
 
+## 1. Setting up HTML
+A skelleton composed of: header, main>players sections+gameboard, and footer.
+
+## 2. Function Factorys and Modules
+
+As this is part of the project, I had some troubles understanding what is the best practice to do this and keep code private.
+
+My answers are: to keep code private in a factory, you should return functions that give you access to the variables of the object. That way it can not be changed. You may put a `object.key="value" ` on the console, but that is on window level, If my code runs using the value returned from the functions of the object, the original value stays safe.
+
+With the modules, something is similar, but as this is a IIEF you are not able to change anything anyways.
+
+My best practice till now is to write the code on window scope, test it, see whats wrong, and then wrap it in a function (for factory) or a IIEF (for module). Thats the way.
+
+## 3. Better commits
+
+After reading a lot of material about what a good commit is, I'm trying to write using the best practices for the commits, imperative writing and all that. This way I hope that commits are clearer and my communication skills improve. Also, writing the commits in VScode is so different from everything else. No more -m flag for commits.
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //variables for testing.
 let currentPlayer = {};
-currentPlayer.marker = "Q";
+//currentPlayer.marker = "Q";
 
 //Div Objects Board - module
 const battlefield = (function () {
@@ -38,7 +38,7 @@ const battlefield = (function () {
     return x == 1 ? picard() : janeway();
   };
 
-  return {images}
+  return { images };
 })();
 
 // eventlisteners and functions
@@ -50,51 +50,55 @@ const eventListenerDivs = (() => {
       if (divs[i].firstChild) {
       } else {
         let p = document.createElement("p");
-        p.innerText = currentPlayer.marker;
+        p.innerText = currentPlayer.getMarker();
         p.classList.add("playersChoice");
         p.id = "p" + [i];
         divs[i].appendChild(p);
-        //I should append the players swap here.
-        //add a check for winning here also nums of ps go
-        //from 0 to 8
+        currentPlayer.pushChoice(i)
+        //check for victory
+        game.swapPlayer();
       }
     });
   }
-  return { divs};
+  return { divs };
 })();
 
-const createPlayer=(()=>{ 
-  let form=document.getElementById("form")
-  form.addEventListener("submit",(event)=>{
-    event.preventDefault()
-    let name1=document.getElementById("name1").value
-    let name2=document.getElementById("name2").value
-    player1=Player(name1,"Q");
-    player2=Player(name2,"X");
-    let nameboard1=document.getElementById("players1name");
-    nameboard1.innerText=player1.getName()
-    let nameboard2=document.getElementById("players2name");
-    nameboard2.innerText=player2.getName()
-    form.classList.add("hidden")
-    battlefield.images()
-    //win count
-  })
+//creating players
+
+const createPlayer = (() => {
+  let form = document.getElementById("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let name1 = document.getElementById("name1").value;
+    let name2 = document.getElementById("name2").value;
+    player1 = Player(name1, "Q");
+    player2 = Player(name2, "X");
+    let nameboard1 = document.getElementById("players1name");
+    nameboard1.innerText = player1.getName();
+    let nameboard2 = document.getElementById("players2name");
+    nameboard2.innerText = player2.getName();
+    form.classList.add("hidden");
+    battlefield.images();
+    let score1 = document.getElementById("score1");
+    let score2 = document.getElementById("score2");
+    score1.innerText = `Score: ${player1.getWinCount()}`;
+    score2.innerText = `Score: ${player2.getWinCount()}`;
+    game.swapPlayer();
+  });
 })();
-
-
 
 //Players Object Factory Function
 
 function Player(name, marker) {
   let playerSquares = [];
-  let winCount = [];
+  let winCount = 0;
   let isHuman = true;
 
   const getName = () => {
     return name;
   };
   const getWinCount = () => {
-    return winCount;
+    return winCount.length;
   };
   const getPlayerSquares = () => {
     return playerSquares;
@@ -102,16 +106,49 @@ function Player(name, marker) {
   const getMarker = () => {
     return marker;
   };
-  return { getMarker, getName, getWinCount, isHuman, getPlayerSquares };
+  const pushChoice = (number) => {
+    playerSquares.push(number);
+  };
+  const addtoWinning=()=>{
+    winCount+=1;
+  }
+  const resetPlayerSquares = () => {
+    playerSquares = [];
+  };
+  return {
+    addtoWinning,
+    getMarker,
+    getName,
+    getWinCount,
+    pushChoice,
+    isHuman,
+    getPlayerSquares,
+    resetPlayerSquares,
+  };
 }
-
-// handling form
 
 // game controller (game steps)
 
 const game = (() => {
+  const swapPlayer = () => {
+    return currentPlayer == player1
+      ? (currentPlayer = player2)
+      : (currentPlayer = player1);
+  };
 
-
-
-  return {};
+  return { swapPlayer };
 })();
+
+const checkForVictory = () => {
+  let players=[player1,player2];
+  //using an array to see the index of elements
+  //to define the winning cases
+  //checking one by one of the two players array
+  //and calling victory in top of the victorius one.
+
+  //winning cases = 0,1,2 - 3,4,5 - 6,7,8
+  //0,3,6 - 1,4,7 - 2,5,8
+  //0,4,8 - 2,4,6
+
+  
+};
